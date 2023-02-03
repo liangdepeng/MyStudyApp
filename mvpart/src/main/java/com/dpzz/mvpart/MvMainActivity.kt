2ffeedf8a26069764139f4d6a56889669021615f
liangdp <liangdp@ct108.com>
2023-02-03 11:32:46 +0800

    movie test module commit

---
+++ mvpart/src/main/java/com/dpzz/mvpart/MvMainActivity.kt
@@ -0,0 +1,20 @@
+package com.dpzz.mvpart
+
+import android.os.Bundle
+import com.dpzz.lib_base.BaseActivity
+import com.dpzz.mvpart.databinding.ActivityMvMainBinding
+
+class MvMainActivity : BaseActivity<ActivityMvMainBinding>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+
+    }
+}
